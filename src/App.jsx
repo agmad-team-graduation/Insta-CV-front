fe53c11d8eeead1afb126db3e1a669025be2d7bf
Mohandmagdy <mohandmagdii45@gmail.com>
2025-04-19 20:24:26 +0200

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router , Route, Routes} from 'react-router';
+import Navbar from "./navbar";
+import './App.css'
+import { CardWithForm } from './card';
+
+
+function App() {
+  return (
+    <div className="app">
+      <p>ES7aaaaaaaaaaa</p>
+      <CardWithForm />
+    </div>
+  );
+}
+
+
+export default App
