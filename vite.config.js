1fd3ec7c748468070bed253d61e0a2413f326061
Osama Maher <osama392maher@gmail.com>
2025-06-12 01:29:12 +0300

    Remove duplicate import statement for defineConfig and loadEnv in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
-import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig(({ mode }) => {
   // load .env, .env.development, etc.
