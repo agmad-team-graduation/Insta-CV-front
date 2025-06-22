a162cb6db0ab10a601a2a8f7275a4ab281ef9f5b
Youssef Morad <youssefmohammed747@gmail.com>
2025-06-23 02:34:51 +0300

    fixed google login + bug fix

--- src/common/components/layout/Sidebar.jsx
+++ src/common/components/layout/Sidebar.jsx
@@ -150,7 +150,7 @@ const Sidebar = () => {
                 <AvatarImage src={userPhoto} alt={displayUser.name} className="object-cover" />
               ) : (
                 <AvatarFallback className="bg-gradient-to-br from-blue-500 to-indigo-600 text-white font-semibold text-lg">
-                  {displayUser.name.charAt(0).toUpperCase()}
+                  {displayUser.name?.charAt(0).toUpperCase()}
                 </AvatarFallback>
               )}
             </Avatar>
